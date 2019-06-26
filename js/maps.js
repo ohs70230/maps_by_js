30229e2e08daec2e0896b76974000bc85e56a47a
ohs70230 <taroo1512@gmail.com>
2019-06-26 13:37:18 +0900

    マーカー一括削除・InfoWindows機能以外実装

---
+++ js/maps.js
@@ -0,0 +1,63 @@
+function getMap() {
+	var latlng = new google.maps.LatLng(34.699875, 135.493032)
+	var opts = {
+		zoom: 14,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	map = new google.maps.Map(document.getElementById('map'), opts)
+
+	// マーカーの配列
+	markers = []
+	marker = new google.maps.Marker({
+		position: latlng,
+		map: map,
+	})
+	markers.push(marker)
+}
+
+function geocoding() {
+	geocoder = new google.maps.Geocoder()
+	geocoder.geocode(
+		{'address': $("[name='geocode']").val(), 'language': 'ja'},
+		function (results, status) {						// geocodeデータ取得成功
+			if (status == google.maps.GeocoderStatus.OK) {
+				console.log('map読み込み(検索) 成功')
+				var latlng = results[0].geometry.location	// 取得した座標
+
+				// マーカー追加
+				marker = new google.maps.Marker({
+					position: latlng,
+					map: map,
+				})
+				markers.push(marker)
+
+				// google.maps.Map()コンストラクタに定義されているsetCenter()メソッドで、geocodeから出力した座標latlngを地図の中心点に設定する。
+				map.setCenter(latlng)
+			}
+			else {											// geocodeデータ取得失敗
+				console.log('map読み込み失敗。APIキーに問題あり？')
+			}
+		}
+	)
+}
+
+function maptypeSwitch() {
+	switch ($("[name='maptype']").val()) {
+		case 'SATELLITE':
+			var opt = {mapTypeId: google.maps.MapTypeId.SATELLITE}
+			break
+		case 'HYBRID':
+			var opt = {mapTypeId: google.maps.MapTypeId.SATELLITE}
+			break
+		case 'TERRAIN':
+			var opt = {mapTypeId: google.maps.MapTypeId.SATELLITE}
+			break
+		case 'ROADMAP':
+		default:
+			var opt = {mapTypeId: google.maps.MapTypeId.ROADMAP}
+	}
+
+	// オプション変更を適用
+	map.setOptions(opt);
+}
